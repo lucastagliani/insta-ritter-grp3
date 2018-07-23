f0fb0254144e1f886c677c6283ad680fc936077d
Lucas Tagliani <lucas.tagliani@zenvia.com>
2018-07-23 16:16:53 -0300

    first commit: getPhotos and getPhotoById working

---
+++ handler.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// RUN LOCAL: serverless invoke local --function photos
+module.exports.photos = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      path: '/photos',
+      photos: [
+        {
+          id: 1,
+          uri: 'https://s2.images.com/1',
+          photo: '/photos/1'
+        },
+        {
+          id: 2,
+          uri: 'https://s2.images.com/1',
+          photo: '/photos/2'
+        }
+      ]
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+// RUN LOCAL: serverless invoke local --function photoById -path data.json
+module.exports.photoById = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        id: event['pathParameters']['photo_id'],
+        uri : 'https://s3.imagens.com/'+event['pathParameters']['photo_id'],
+        photo : '/photos/'+event['pathParameters']['photo_id'],
+      }
+  )};
+
+  callback(null, response);
+};
