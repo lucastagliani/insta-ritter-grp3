0e40e201239bbe6b732aafb8e72e42c553e84cea
Renan Drabach <rmdrabach+github@gmail.com>
2018-07-27 00:57:47 -0300

    Upload de foto para o S3.

--- src/back/app/handler.js
+++ src/back/app/handler.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const s3 = AWS.s3();
+const moment = require('moment');
+const fileType = require('file-type');
+
 // RUN LOCAL: serverless invoke local --function photos
 module.exports.photos = (event, context, callback) => {
   const response = {
@@ -34,18 +39,69 @@ module.exports.photoById = (event, context, callback) => {
     body: JSON.stringify(
       {
         id: event['pathParameters']['photo_id'],
-        uri : 'https://s3.imagens.com/'+event['pathParameters']['photo_id'],
-        photo : '/photos/'+event['pathParameters']['photo_id'],
+        uri: 'https://s3.imagens.com/' + event['pathParameters']['photo_id'],
+        photo: '/photos/' + event['pathParameters']['photo_id'],
       }
-  )};
-
-  callback(null, response);
-};
-
-module.exports.photo = (event, context, callback) => {
-  const response = {
-    statusCode: 200
+    )
   };
 
   callback(null, response);
 };
+
+// upload
+module.exports.photo = (event, context, callback) => {
+  let request = event.body;
+  let base64String = request.base64String;
+  let buffer = new Buffer(base64String, 'base64');
+  let fileMime = fileType(buffer)
+  if (fileMime == null) {
+    return context.fail('The string supplied is not a file type.');
+  }
+
+  let file = getFile(fileMime, buffer);
+  let params = file.params;
+
+  s3.putObject(params, function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    return console.log('File URL', file.full_path);
+  });
+
+
+  // const response = {
+  //   statusCode: 200
+  // };
+
+  // callback(null, response);
+};
+
+let getFile = function (fileMime, buffer) {
+  let fileExt = fileMime.ext;
+  let hash = sha1(new Buffer(new Date().toString()));
+  let now = moment().format('YYYY-MM-DD HH:mm:ss');
+
+  let filePath = hash + '/';
+  let fileName = unixTime(now) + '.' + fileExt;
+  let fileFullName = filePath + fileName;
+  let fileFullPath = 'OUR BUCKET PATH' + fileFullName;
+
+  let params = {
+    Bucket: 'instaritter',
+    Key: fileFullname + fileExt, //this is simply the filename and the extensions e.g fileFullname + fileExt
+    Body: buffer
+  }
+
+  let uploadFile = {
+    size: buffer.toString('ascii').length,
+    type: fileMime.mime,
+    name: fileName,
+    full_path: fileFullPath
+  }
+
+  return {
+    'params': params,
+    'uploadFile': uploadFile
+  }
+}
